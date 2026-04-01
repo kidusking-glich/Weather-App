b5535fcff7b55fbc98c50207b2ed666dc738fd35
kidusking-glich <abelyohannes69@gmail.com>
2026-04-01 09:33:02 +0300

    Adding validation for the city if idoesnt exit

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ API_KEY = "7d1f1"
 @app.route("/", methods=["GET", "POST"])
 def index():
     weather = None
+    error = None
 
     if request.method == "POST":
         city = request.form["city"]
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
         response = requests.get(url)
         data = response.json()
-        if data["cod"] == 200:
+        
+        if str(data.get("cod")) == "200":
             weather = {
                 "city": city,
                 "temp": data["main"]["temp"],
@@ -25,12 +27,13 @@ def index():
                 "country": data["sys"]["country"],
                 "feels_like": data["main"]["feels_like"]
             }
-        if weather:
             if "rain" in weather["description"]:
                 bg = "bg-primary"
             elif "clear" in weather["description"]:
                 bg = "bg-warning" 
+        else:
+            error = data.get("message", "City not found.").capitalize()
 
-    return render_template("index.html", weather=weather)
+    return render_template("index.html", weather=weather, error=error)
 if __name__ == "__main__":
     app.run(debug=True)
